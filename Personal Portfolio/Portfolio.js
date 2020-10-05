4f61d4daa63e8bcb462f225747887b68fd70dbe0
Christopher Stevers <christopher.stevers1@gmail>
2020-10-05 08:36:34 -0400

    Created porfolio site and built header and nav.

---
+++ Personal Portfolio/Portfolio.js
@@ -0,0 +1,4 @@
+function displayList(){document.getElementById("menu").style.visibility = "visible";
+document.getElementById("icon").style.visibility = "hidden";}
+function hideList(){document.getElementById("icon").style.visibility = "visible";
+document.getElementById("menu").style.visibility = "hidden";}
